bc14dab7979d95a1749f39f97141e3632ebdebf4
elid venega <venegaelid@gmail.com>
2021-03-29 23:08:50 -0700

    project restarted

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const slider = document.querySelector('.slider-container'),
+    slides = Array.from(document.querySelectorAll('.slide'));
+
+let isDragging = false,
+    startPos = 0,
+    currentTranslate = 0,
+    prevTranslate = 0,
+    animationID = 0,
+    currentIndex = 0
+
+slides.forEach((slide,index)=> {
+     const slideImage = slide.querySelector   
+}) 
